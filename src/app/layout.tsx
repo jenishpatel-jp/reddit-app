8c0029f551b9bf34ba42ed8e0fec52907a8d6bd0
Jenish <jpat534@gmail.com>
2024-04-09 15:28:21 +1000

    added reddit favicon icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,12 @@ export default function RootLayout({
     <html lang="en">
       <head>
       <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link
+        rel="icon"
+        href="/icon?<generated>"
+        type="image/<generated>"
+        sizes="<generated>"
+      />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
